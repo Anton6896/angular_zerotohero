a7130c13dbc34b641bc878e18ecfcbf664f30b05
anton <anton-r@live.com>
2021-12-16 19:14:00 +0200

    finished basics

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+### course position 
+[location](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture)
+
+
 * bootstrap    
 npm i bootstrap -> include this bootstrap in the angular.json file as additional stile  
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,9 +8,40 @@ import {Component, OnInit} from '@angular/core';
 export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverNme: string = 'SomeName';
+  isEnabled: boolean = false;
+  isChanges: boolean = true;
+  serverCreationStatus: string = 'initial string';
+  buttonName: string = 'click me'
+  showPast: boolean = false;
+  serverStatus: string = Math.random() > 0.5 ? 'offline' : 'online'
+  availbleData: string[] = ['data1', 'data2']
+
+
+  onCreateServer() {
+    this.isChanges = !this.isChanges;
+    this.serverCreationStatus = this.isChanges ? '-------' : this.serverNme
+  }
+
+  onUpdateName(event: Event) {
+    this.serverCreationStatus = event.target['value']
+  }
+
+  onShowPast() {
+    this.showPast = !this.showPast
+  }
 
   getServerName() {
     return this.serverNme
+  }
+
+  onSetServerStatus() {
+    this.availbleData.push('newone')
+    console.log(this.availbleData)
+    this.serverStatus = 'online'
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red'
   }
 
   constructor() {
