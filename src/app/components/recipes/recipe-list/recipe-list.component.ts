c9121c7fe331db57dbfba623c70fb65b13d65d3e
mainPC <anton-r@live.com>
2021-12-25 14:53:20 +0200

    on click event added

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -17,7 +17,7 @@ export class RecipeListComponent implements OnInit {
     ),
     new Recipe('recName2',
       'description 2',
-      'https://static01.nyt.com/images/2021/12/13/dining/MC-Classic-Potato-Latkes/merlin_199179672_13ff5d80-3b33-46de-9904-0ccaabe97917-articleLarge.jpg'
+      'https://www.photoblog.com/learn/wp-content/uploads/2018/11/weeattogether.com-2-990x556.jpg'
     ),
   ]
 
